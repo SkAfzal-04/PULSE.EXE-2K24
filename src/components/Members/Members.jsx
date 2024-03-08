93a8aeedf2d18db9258ad9c9d2dd3f8deba60364
SkAfzal-04 <skmahammadjinnur@gmail.com>
2024-03-08 22:06:20 +0530

    member update

--- src/components/Members/Members.jsx
+++ src/components/Members/Members.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo } from "react";
 import "./Members.css";
-import member1 from "../../members/Sk Mahammad Afzal.jpeg";
+import member1 from "../../members/Hridhwesh Maity.jpg";
 import member2 from "../../members/Arup Maity.jpeg";
 import member3 from "../../members/Sanchari Pandey.jpg";
 import member4 from "../../members/Avik Sen.jpeg";
@@ -9,7 +9,7 @@ import member6 from "../../members/Dipti Bhowmik.jpeg";
 import member7 from "../../members/Tiyasa Saha.jpeg";
 import member8 from "../../members/Arijit Karmakar.jpg";
 import member9 from "../../members/Ayan Saha.jpg";
-import member10 from "../../members/Hridhwesh Maity.jpg";
+import member10 from "../../members/Sk Mahammad Afzal.jpeg";
 import member11 from "../../members/Sreejit Chakraborty.jpg";
 import member12 from "../../members/Bishnu Bhakat.jpg";
 import member13 from "../../members/Sreya Dhar.jpg";
@@ -22,16 +22,16 @@ import member17 from "../../members/Sarthak Mukherjee.jpeg";
 const Members = () => {
   const membersArr = useMemo(
     () => [
-      { id: 1, name: "Sk Mahammad Afzal", image: member1, whatsappLink: "https://wa.me/+919679186577" },
+      { id: 1, name: "Hridhwesh Maity", image: member1, whatsappLink: "https://wa.me/+917688085816" },
       { id: 2, name: "Arup Maity", image: member2, whatsappLink: "https://wa.me/+919123927177" },
       { id: 3, name: "Sanchari Pandey", image: member3, whatsappLink: "https://wa.me/+919749815573" },
       { id: 4, name: "Avik Sen", image: member4, whatsappLink: "https://wa.me/+917980727150" },
       { id: 5, name: "Kushal Singha", image: member5, whatsappLink: "https://wa.me/+919339440674" },
-      { id: 6, name: "Dipti Bhowmik", image: member6, whatsappLink: "https://wa.me/+919330066202" },
+      { id: 6, name: "Dipti Bhowmik", image: member6, whatsappLink: "https://wa.me/+919064148052" },
       { id: 7, name: "Tiyasa Saha", image: member7, whatsappLink: "https://wa.me/+919433077890" },
       { id: 8, name: "Arijit Karmakar", image: member8, whatsappLink: "https://wa.me/+919875347630" },
       { id: 9, name: "Ayan Saha", image: member9, whatsappLink: "https://wa.me/+916290107258" },
-      { id: 10, name: "Hridhwesh Maity", image: member10, whatsappLink: "https://wa.me/+917688085816" },
+      { id: 10, name: "Sk Mahammad Afzal", image: member10, whatsappLink: "https://wa.me/+919679186577" },
       { id: 11, name: "Sreejit Chakraborty", image: member11, whatsappLink: "https://wa.me/+919831526476" },
       { id: 12, name: "Bishnu Bhakat", image: member12, whatsappLink: "https://wa.me/+919330809352" },
       { id: 13, name: "Sreya Dhar", image: member13, whatsappLink: "https://wa.me/+919748375528" },
