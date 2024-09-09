fe06ba0e5f885d2e3921607cb8aa83291eb4963c
SkAfzal-04 <skmahammadjinnur@gmail.com>
2024-09-09 15:42:55 +0530

    Initial commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
     let interval;
 
     const startTimer = () => {
-        const countDownDate = new Date("April 10,2024 09:30:00").getTime();
+        const countDownDate = new Date("August 31,2024 09:30:00").getTime();
 
         interval = setInterval(() => {
             const now = new Date().getTime();
